6c14252446acced9eb17d510fc9ac4cea1ab048a
Jalilov <javohirjalilov777@gmail.com>
2021-06-12 18:28:19 +0500

    drawing line detection image

--- main.py
+++ main.py
@@ -19,11 +19,43 @@ HSV_ROI = cv2.cvtColor(ROI_img,cv2.COLOR_BGR2HSV)
 thresh = cv2.inRange(HSV_ROI,line_lower,line_upper)
 # adge = cv2.Canny(thresh,300,400)
 linesP = cv2.HoughLinesP(thresh,1,np.pi/180,threshold=50,minLineLength=40,maxLineGap=100)
+
+rightSlope = []
+leftSlope = []
+rightIntercept = []
+leftIntercept = []
+
 for line in linesP:
     for (x1,y1,x2,y2) in line:
-        cv2.line(frame,(x1,y1),(x2,y2),(255,0,0),2)
+        slope = (y2-y1)/(x2-x1)
+        if slope > 0.2:
+            if x1 > 300:
+                y_intercept = y2 - slope*x2
+                rightSlope.append(slope)
+                rightIntercept.append(y_intercept)
+            else:
+                None
+        elif slope < -0.2:
+            if x1 < 400:
+                y_intercept = y2 - slope*x2
+                leftSlope.append(slope)
+                leftIntercept.append(y_intercept)
 
-print(len(linesP))
+leftavgSlope = np.mean(leftSlope)
+leftavgIntercept = np.mean(leftIntercept)
+rightavgSlope = np.mean(rightSlope)
+rightavgIntercept = np.mean(rightIntercept)
+
+left_line_x1 = int((0.65*y - leftavgIntercept)/leftavgSlope)
+left_line_x2 = int((y - leftavgIntercept)/leftavgSlope)
+
+right_line_x1 = int((0.65*y - rightavgIntercept)/rightavgSlope)
+right_line_x2 = int((y - rightavgIntercept)/rightavgSlope)
+
+cv2.line(frame,(left_line_x1,int(0.65*y)),(left_line_x2,y),(255,0,0),3)
+cv2.line(frame,(right_line_x1,int(0.65*y)),(right_line_x2,y),(0,255,0),3)
+
+print(leftavgIntercept)
 cv2.imshow('image',frame)
 cv2.waitKey(0)
 
